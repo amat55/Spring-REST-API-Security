f18a0c3bafd176526abacde812ec597011b7a583
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-08-07 13:44:13 +0200

    Created REST api Security

---
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+}
+// network security - web app fare wall - kali linux- web app firewall
